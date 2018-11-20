2358c6bee67d6efaf6918484a19a973ecd7dad9e
exitinfo <wngus_569@naver.com>
2018-11-19 20:52:05 -0800

    Python

---
+++ gugu.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+#for i in range(2, 10):
+#	for j in range(1, 10):
+#		print(str(i)+ "*"+ str(j)+ "="+ str(i*j))
+i=2
+j=1
+while (i < 10):
+	j=1
+	while (j < 10):
+		print(str(i)+ "*"+ str(j)+ "="+ str(i*j))
+		j+=1
+	i+=1
+
+
